df9b59ff03dcfa7e7b3ffcb0ec680427efcbc11b
Diptej Saner <sanerdiptej@gmail.com>
2016-05-12 21:37:54 +0530

    Revert "Remove unnecessary comments"
    This reverts commit fb564307c20d2be9de3ad7edb1741855191636af.

--- testIter.py
+++ testIter.py
@@ -1,11 +1,31 @@
 from lxml import etree
 import lxml.html
 import re
-import datetime
 
 #with open("DataUsage.txt", "r") as dfile:
 #	content = dfile.read()
 
+content1 = """<table>
+			 	<tr>
+			 		<td class="labeltext" align='center' nowrap>1351265</td>								
+					<td class="labeltext" align='center' nowrap>HD 2 STREAM 50MB Qtrly U</td>
+					<td class="labeltext" align='center'>08/05/2016</td>			
+					<td class="labeltext" align='right'>1279116</td>			
+					<td class="labeltext" align='right'>5585572</td>	
+					<td class="labeltext" align='right'>6864688</td>			
+					<td class="labeltext" align='right'>6.547</td>					
+					<td class="labeltext" align='right'>0</td>
+			 	</tr>
+			 </table>"""
+
+content2 = """sfasfasdfasf
+			<table>
+  			 <tr>	<th>Event</th>	<th>Start Date</th>	<th>End Date</th>	</tr>
+  			 <tr>	<td>a</td>	<td>b</td>	<td>c</td>	</tr>
+  			 <tr>	<td>d</td>	<td>e</td>	<td>f</td>	</tr>
+  			 <tr>	<td>g</td>	<td>h</td>	<td>i</td>	</tr>
+			</table>
+		"""
 with open("DataUsage.txt", "r") as dfile:
 	content = dfile.read()
 
@@ -13,46 +33,32 @@ content_table = re.findall(r'<table border=1.*?</table>', content)
 
 root = lxml.html.fromstring(content)
 usage_raw = root.xpath('//table//tr[2]//table//tr//td/text()')
+print(root.xpath('//table//tr[2]//table//tr//td/text()'))
+
+'''
+parser = etree.HTMLParser()
+tree = etree.parse(content_table[0], parser)
+
+result = etree.tostring(tree.getroot(), pretty_print=True, method="html")
+
+print(result)
+
+'''
+
+'''
+table = etree.XML(content_table[0])
+#print(table)
+rows = iter(table)
+
+headers = [col.text for col in next(rows)]
+print(headers)
+for row in rows:
+    values = [col.text for col in row]
+    print(dict(zip(headers, values)))
+'''
 
 usage_matrix = []
 usage_matrix = [usage_raw[x:x+8] for x in range(0, len(usage_raw), 8)]
-dates = []
 
 for list in usage_matrix:
 	print(list)
-	dates.append(datetime.datetime.strptime(list[2], '%d/%m/20%y').date())
-
-date1 = datetime.datetime.strptime('10/05/2016', '%d/%m/20%y').date()
-date2 = datetime.datetime.strptime('12/05/2016', '%d/%m/20%y').date()
-
-print(date1)
-print(date2)
-
-if(date2 > date1):
-	print(date2 - date1)
-
-i = 0
-index = 0
-upload = 0
-download = 0
-'''for row in usage_matrix:
-	usage += float(row[6])
-	day = row[2].split('/')
-	if(day[0] == '07'):
-		index = i
-		break
-	i += 1	
-'''
-days = 0
-usage = 0
-for row in usage_matrix:
-	upload 		+= float(row[3])/1000000
-	download 	+= float(row[4])/1000000
-	usage += float(row[6])
-	date = row[2].split('/')
-	days += 1
-
-	if(date[0] == '07'):
-		break
-print("total : {0}".format(usage))
-print("Upload = {0} Download = {1} Total usage : {2} MB in {3} days.".format(upload, download, (upload+download), days))
